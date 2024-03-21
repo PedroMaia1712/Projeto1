9fa563a03a00eb258c904dea08706905a3c07b05
PedroMaia1712 <Maiapedrohenrique@gmail.com>
2024-03-21 20:01:41 -0300

    alterando limite para 100

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import AlunoUpdateView
 
 urlpatterns = [
-    path('', views.alunoView, name='aluno-lista')
+    path('', views.alunoView, name='aluno-lista'),
+
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+
+    path('alunoContact', views.contact_view, name='contato'),
+    path('aluno/create/', views.AlunoCreateViews.as_view(), name='aluno-create'),
+    path('aluno/<int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-update'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Aluno
 from django.http import HttpResponse
 
@@ -6,3 +6,40 @@ from django.http import HttpResponse
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request, 'main/alunos.html', {'alunos_list': alunos_list})
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html', {'aluno':aluno})
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print('name:', name)
+        print('Email:', email)
+        print('Message:', message)
+    return render (request, 'main/contact.html')
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import AlunoForm
+
+class AlunoCreateViews(CreateView):
+    model = Aluno
+    form_class = AlunoForm
+    success_url = reverse_lazy('aluno-lista')
+    template_name = 'main/aluno_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+class AlunoUpdateView(UpdateView):
+    model = Aluno
+    form_class = AlunoForm
+    template_name = 'main/aluno_form.html'
+    success_url = reverse_lazy('aluno-lista')
+
+    
